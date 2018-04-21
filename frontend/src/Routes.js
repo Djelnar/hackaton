6f39501c3bf5d3401258c2d20997cc9e57625fe7
Elnar Ibragimov <djelnar@gmail.com>
2018-04-22 02:44:05 +0300

    f

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,6 +5,8 @@ import { SelectSize } from './pages/select-size';
 import { Big } from './pages/big';
 import { Small } from './pages/small';
 import { BigPage } from './pages/big/page';
+import { SmallPage } from './pages/small/page';
+import { CreateSmall } from './pages/small/create';
 
 
 export const Routes = () => (
@@ -13,12 +15,17 @@ export const Routes = () => (
     <Route render={() => <SelectSize />} path='/select-size' exact />
     <Route render={() => <Big />} path='/big-form' exact />
     <Route render={() => <Small />} path='/small-form' exact />
+    <Route render={() => <CreateSmall />} path='/smalleventcreate' exact />
     <Route
       render={({ match }) => <BigPage id={match.params.id} />}
       path='/bigevent/:id'
       exact
     />
-    <Route render={() => 'dafault'} path='/' exact />
+    <Route
+      render={({ match }) => <SmallPage id={match.params.id} />}
+      path='/smallevent/:id'
+      exact
+    />
     <Redirect to='/auth' />
   </Switch>
 )
